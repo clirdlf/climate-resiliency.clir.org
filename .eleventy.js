47a6c046a4079753b3add90898b65c64fd3c7417
Wayne Graham <wgraham@clir.org>
2023-09-19 12:26:57 -0400

    Update template and content

--- .eleventy.js
+++ .eleventy.js
@@ -2,12 +2,14 @@ const path = require("path");
 
 const { DateTime } = require("luxon");
 const markdownItAnchor = require("markdown-it-anchor");
-const markdownItAttrs = require('markdown-it-attrs');
-const markdownItRenderer = require('markdown-it')('commonmark');
+const markdownItAttrs = require("markdown-it-attrs");
+const markdownItRenderer = require("markdown-it")("commonmark");
 
-const excerpt = require('eleventy-plugin-excerpt'); //https://github.com/psalaets/eleventy-plugin-excerpt
-const schema = require('@quasibit/eleventy-plugin-schema'); //https://www.maxivanov.io/add-structured-data-to-eleventy-blog/
-const emojiReadTime = require('@11tyrocks/eleventy-plugin-emoji-readtime'); // https://github.com/5t3ph/eleventy-plugin-emoji-readtime
+const CleanCSS = require("clean-css"); // https://www.11ty.dev/docs/quicktips/inline-css/
+
+const excerpt = require("eleventy-plugin-excerpt"); //https://github.com/psalaets/eleventy-plugin-excerpt
+const schema = require("@quasibit/eleventy-plugin-schema"); //https://www.maxivanov.io/add-structured-data-to-eleventy-blog/
+const emojiReadTime = require("@11tyrocks/eleventy-plugin-emoji-readtime"); // https://github.com/5t3ph/eleventy-plugin-emoji-readtime
 
 // official plugins
 const pluginRss = require("@11ty/eleventy-plugin-rss");
@@ -21,79 +23,85 @@ const Image = require("@11ty/eleventy-img"); // https://www.11ty.dev/docs/plugin
 const pluginDrafts = require("./eleventy.config.drafts.js"); // https://github.com/11ty/eleventy-base-blog/blob/main/eleventy.config.js#LL10C1-L10C61
 
 // https://github.com/11ty/eleventy-img/issues/46#issuecomment-766054646
-function generateImages(src, widths){
-	let source = path.join(__dirname, "src/" , src);
+function generateImages(src, widths) {
+	let source = path.join(__dirname, "src/", src);
 	let options = {
 		widths: widths,
-		formats: ['png',],
+		formats: ["png"],
 		outputDir: "./docs/assets/images/",
 		urlPath: "/assets/images/",
 		useCache: true,
 		sharpJpegOptions: {
 			quality: 99,
-			progressive: true
-		}
+			progressive: true,
+		},
 	};
 	// genrate images, ! dont wait
 	Image(source, options);
 	// get metadata even the image are not fully generated
 	return Image.statsSync(source, options);
 }
- 
-function imageCssBackground (src, selector, widths){
+
+function imageCssBackground(src, selector, widths) {
 	const metadata = generateImages(src, widths);
-	let markup = [`${selector} { background-image: url(${metadata.jpeg[0].url});} `];
+	let markup = [
+		`${selector} { background-image: url(${metadata.jpeg[0].url});} `,
+	];
 	// i use always jpeg for backgrounds
 	metadata.jpeg.slice(1).forEach((image, idx) => {
-		markup.push(`@media (min-width: ${metadata.jpeg[idx].width}px) { ${selector} {background-image: url(${image.url});}}`);
+		markup.push(
+			`@media (min-width: ${metadata.jpeg[idx].width}px) { ${selector} {background-image: url(${image.url});}}`
+		);
 	});
 	return markup.join("");
 }
 
 module.exports = (eleventyConfig) => {
-    // Copy the "assets" directory to the compiled "_site" folder.
-    eleventyConfig.addPassthroughCopy('src/assets');
-	eleventyConfig.addPassthroughCopy('src/robots.txt');
-    eleventyConfig.addPassthroughCopy('site.webmanifest');
-    eleventyConfig.addPassthroughCopy('icon.svg');
-    eleventyConfig.addPassthroughCopy('CNAME');
-
+	// Copy the "assets" directory to the compiled "_site" folder.
+	eleventyConfig.addPassthroughCopy("src/assets");
+	eleventyConfig.addPassthroughCopy("src/robots.txt");
+	eleventyConfig.addPassthroughCopy("src/site.webmanifest");
+	eleventyConfig.addPassthroughCopy("src/icon.svg");
+	eleventyConfig.addPassthroughCopy("CNAME");
 
 	eleventyConfig.addPlugin(schema);
 	eleventyConfig.addPlugin(excerpt);
 
-    // Official plugins
+	// Official plugins
 	eleventyConfig.addPlugin(eleventyNavigationPlugin);
-    eleventyConfig.addPlugin(EleventyRenderPlugin);
+	eleventyConfig.addPlugin(EleventyRenderPlugin);
 	eleventyConfig.addPlugin(EleventyHtmlBasePlugin);
-    eleventyConfig.addPlugin(pluginRss);
-    eleventyConfig.addPlugin(pluginBundle);
+	eleventyConfig.addPlugin(pluginRss);
+	eleventyConfig.addPlugin(pluginBundle);
 	eleventyConfig.addPlugin(pluginDrafts);
 
-
-    // watch CSS files for changes
-    eleventyConfig.setBrowserSyncConfig({
-        files: './_css/**/*.css'
-    });
+	// watch CSS files for changes
+	eleventyConfig.setBrowserSyncConfig({
+		files: "./_css/**/*.css",
+	});
 
 	// https://www.11ty.dev/docs/collections/#getfilteredbytags(-tagname-secondtagname-%5B...%5D-)
-	eleventyConfig.addCollection('news', function(collectionApi){
+	eleventyConfig.addCollection("news", function (collectionApi) {
 		return collectionApi.getFilteredByTags("posts", "news");
 	});
 
 	// cssBackground shortcode
 	eleventyConfig.addShortcode("cssBackground", imageCssBackground);
 
-	// image shortcode 
+	// image shortcode
 	// @see https://www.11ty.dev/docs/plugins/image/
-	eleventyConfig.addShortcode("image", async function(src, alt, css, sizes) {
+	eleventyConfig.addShortcode("image", async function (src, alt, css, sizes) {
 		let metadata = await Image(src, {
 			widths: [300, 600, 720],
 			// widths: [300, 600, 720, "auto"],
 			formats: ["webp", "avif", "jpeg"],
-			urlPath: '/assets/images/optimized/',
-			outputDir: path.join(eleventyConfig.dir.output, "assets", "images", "optimized")
-
+			urlPath: "/assets/images/optimized/",
+			outputDir: path.join(
+				eleventyConfig.dir.output,
+				"assets",
+				"images",
+				"optimized"
+			),
 		});
 		// https://github.com/11ty/eleventy-img/issues/97
 		let imageAttributes = {
@@ -108,14 +116,32 @@ module.exports = (eleventyConfig) => {
 		return Image.generateHTML(metadata, imageAttributes);
 	});
 
-	  // Filters
+	eleventyConfig.addFilter("postDate", (dateObj) => {
+		return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+	});
 
-    // Get the first `n` elements of a collection.
+	// Filters
+	// https://www.11ty.dev/docs/quicktips/inline-css/
+	eleventyConfig.addFilter("cssmin", function (code) {
+		return new CleanCSS({}).minify(code).styles;
+	});
+
+	eleventyConfig.addNunjucksFilter("limit", (arr, limit) =>
+		arr.slice(0, limit)
+	);
+
+	// https://11ty.rocks/eleventyjs/content/#excerpt-filter
+	eleventyConfig.addFilter("excerpt", (post) => {
+		const content = post.replace(/(<([^>]+)>)/gi, "");
+		return content.substr(0, content.lastIndexOf(" ", 200)) + "...";
+	});
+
+	// Get the first `n` elements of a collection.
 	eleventyConfig.addFilter("head", (array, n) => {
-		if(!Array.isArray(array) || array.length === 0) {
+		if (!Array.isArray(array) || array.length === 0) {
 			return [];
 		}
-		if( n < 0 ) {
+		if (n < 0) {
 			return array.slice(n);
 		}
 
@@ -124,20 +150,22 @@ module.exports = (eleventyConfig) => {
 
 	eleventyConfig.addFilter("readableDate", (dateObj, format, zone) => {
 		// Formatting tokens for Luxon: https://moment.github.io/luxon/#/formatting?id=table-of-tokens
-		return DateTime.fromJSDate(dateObj, { zone: zone || "utc" }).toFormat(format || "LLL yyyy");
+		return DateTime.fromJSDate(dateObj, { zone: zone || "utc" }).toFormat(
+			format || "LLL dd, yyyy"
+		);
 	});
 
-	eleventyConfig.addFilter('htmlDateString', (dateObj) => {
+	eleventyConfig.addFilter("htmlDateString", (dateObj) => {
 		// dateObj input: https://html.spec.whatwg.org/multipage/common-microsyntaxes.html#valid-date-string
-		return DateTime.fromJSDate(dateObj, {zone: 'utc'}).toFormat('yyyy-LL-dd');
+		return DateTime.fromJSDate(dateObj, { zone: "utc" }).toFormat("yyyy-LL-dd");
 	});
 
 	// Get the first `n` elements of a collection.
 	eleventyConfig.addFilter("head", (array, n) => {
-		if(!Array.isArray(array) || array.length === 0) {
+		if (!Array.isArray(array) || array.length === 0) {
 			return [];
 		}
-		if( n < 0 ) {
+		if (n < 0) {
 			return array.slice(n);
 		}
 
@@ -145,25 +173,29 @@ module.exports = (eleventyConfig) => {
 	});
 
 	// Return all the tags used in a collection
-	eleventyConfig.addFilter("getAllTags", collection => {
+	eleventyConfig.addFilter("getAllTags", (collection) => {
 		let tagSet = new Set();
-		for(let item of collection) {
-			(item.data.tags || []).forEach(tag => tagSet.add(tag));
+		for (let item of collection) {
+			(item.data.tags || []).forEach((tag) => tagSet.add(tag));
 		}
 		return Array.from(tagSet);
 	});
 
 	eleventyConfig.addFilter("filterTagList", function filterTagList(tags) {
-		return (tags || []).filter(tag => ["all", "nav", "post", "posts"].indexOf(tag) === -1);
+		return (tags || []).filter(
+			(tag) => ["all", "nav", "post", "posts"].indexOf(tag) === -1
+		);
 	});
 
 	// Shortcodes
 	eleventyConfig.addPlugin(emojiReadTime, {
-		emoji: '📖',
+		emoji: "📖",
 	});
 
-    // Customize Markdown library settings:
-    eleventyConfig.amendLibrary("md", mdLib => {
+	eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+
+	// Customize Markdown library settings:
+	eleventyConfig.amendLibrary("md", (mdLib) => {
 		mdLib.use(markdownItAttrs);
 
 		mdLib.use(markdownItAnchor, {
@@ -173,34 +205,29 @@ module.exports = (eleventyConfig) => {
 				symbol: "#",
 				ariaHidden: false,
 			}),
-			level: [1,2,3,4],
-			slugify: eleventyConfig.getFilter("slugify")
+			level: [1, 2, 3, 4],
+			slugify: eleventyConfig.getFilter("slugify"),
 		});
 	});
 
-    // https://github.com/11ty/eleventy/issues/543#issuecomment-1005914243
-    eleventyConfig.addFilter('markdownify', (str) => {
+	// https://github.com/11ty/eleventy/issues/543#issuecomment-1005914243
+	eleventyConfig.addFilter("markdownify", (str) => {
 		return markdownItRenderer.render(str);
-    });
-    
-    return {
-        dir: {
-            input: 'src',
-            output: 'docs',
-            layouts: '_layouts',
-            data: '_data',
-            includes: '_includes',
-        },
-        templateFormats: [
-            'md',
-			'njk',
-			'html',
-            'liquid'
-        ],
+	});
+
+	return {
+		dir: {
+			input: "src",
+			output: "docs",
+			layouts: "_layouts",
+			data: "_data",
+			includes: "_includes",
+		},
+		templateFormats: ["md", "njk", "html", "liquid"],
 
 		// Pre-process *.md files with: (default: `liquid`)
 		markdownTemplateEngine: "njk",
 
-        pathPrefix: '', // omit this line if using custom domain
-    };
+		pathPrefix: "", // omit this line if using custom domain
+	};
 };
---
+++ src/assets/js/script.js
@@ -0,0 +1,88 @@
+// Import the Bootstrap bundle
+//
+// This includes Popper and all of Bootstrap's JS plugins.
+// @see https://github.com/twbs/examples/blob/main/sass-js/js/main.js
+// import "../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js";
+
+document.addEventListener('DOMContentLoaded', function () {
+    const header = document.getElementById('header');
+
+    window.onscroll = function() {
+        if( window.scrollY >= header.offsetTop ) {
+            header.classList.add('nav-bg');
+        } 
+        if( window.scrollY <= header.offsetTop ) {
+            header.classList.remove('nav-bg');
+        } 
+    }
+
+    // TODO: do this in template 
+    const backgrounds = document.querySelectorAll("[data-background]");
+    for (const element of backgrounds) {
+        const backgroundImage = element.dataset.background;
+        element.style.backgroundImage = `url(${backgroundImage})`;
+    }
+
+    const colors = document.querySelectorAll("[data-color]");
+    for (const element of colors) {
+        const backgroundColor = element.dataset.color;
+        element.style.backgroundColor = backgroundColor;
+    }
+
+    const progress = document.querySelectorAll("[data-progress]");
+    for (const element of progress) {
+        const backgroundColor = element.dataset.color;
+        element.style.backgroundColor = backgroundColor;
+    }
+
+    const elements = document.querySelectorAll("[data-progress]");
+    for (const element of elements) {
+        const progress = element.dataset.progress;
+        element.style.bottom = progress;
+    }
+
+    // hero parallax
+    const parallaxBox = document.getElementById('parallax');
+    const c2left = document.getElementById('l2').offsetLeft,
+        c2top = document.getElementById('l2').offsetTop,
+        c3left = document.getElementById('l3').offsetLeft,
+        c3top = document.getElementById('l3').offsetTop,
+        c4left = document.getElementById('l4').offsetLeft,
+        c4top = document.getElementById('l4').offsetTop,
+        c5left = document.getElementById('l5').offsetLeft,
+        c5top = document.getElementById('l5').offsetTop,
+        c6left = document.getElementById('l6').offsetLeft,
+        c6top = document.getElementById('l6').offsetTop,
+        c7left = document.getElementById('l7').offsetLeft,
+        c7top = document.getElementById('l7').offsetTop,
+        c8left = document.getElementById('l8').offsetLeft,
+        c8top = document.getElementById('l8').offsetTop,
+        c9left = document.getElementById('l9').offsetLeft,
+        c9top = document.getElementById('l9').offsetTop;
+
+        parallaxBox.onmousemove = function (event) {
+            event = event || window.event;
+            var x = event.clientX - parallaxBox.offsetLeft,
+                y = event.clientY - parallaxBox.offsetTop;
+
+            /*  mouseParallax('l1', c1left, c1top, x, y, 5); */
+            mouseParallax('l2', c2left, c2top, x, y, 25);
+            mouseParallax('l3', c3left, c3top, x, y, 20);
+            mouseParallax('l4', c4left, c4top, x, y, 35);
+            mouseParallax('l5', c5left, c5top, x, y, 30);
+            mouseParallax('l6', c6left, c6top, x, y, 45);
+            mouseParallax('l7', c7left, c7top, x, y, 30);
+            mouseParallax('l8', c8left, c8top, x, y, 25);
+            mouseParallax('l9', c9left, c9top, x, y, 40);
+        };
+
+        function mouseParallax(id, left, top, mouseX, mouseY, speed) {
+            var obj = document.getElementById(id);
+            var parentObj = obj.parentNode,
+                containerWidth = parseInt(parentObj.offsetWidth),
+                containerHeight = parseInt(parentObj.offsetHeight);
+            obj.style.left = left - (((mouseX - (parseInt(obj.offsetWidth) / 2 + left)) / containerWidth) * speed) + 'px';
+            obj.style.top = top - (((mouseY - (parseInt(obj.offsetHeight) / 2 + top)) / containerHeight) * speed) + 'px';
+        }
+
+}, false);
